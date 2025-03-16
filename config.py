a00d1cdf21238874054ca80f78c39d8dcbbbce4d
SHIVA <151495276+FRANCIUM444@users.noreply.github.com>
2025-03-16 14:14:49 +0530

    Update config.py
    Signed-off-by: SHIVA <151495276+FRANCIUM444@users.noreply.github.com>

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "6369434417").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/FRANCIUM444/SONA_MUSIC_BOT")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/FRANCIUM444/SONA_MUSIC_BOTT")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
