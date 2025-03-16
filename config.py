9c8136e5eb3133c4d86c5911544ae717cef93e7c
SHIVA <151495276+FRANCIUM444@users.noreply.github.com>
2025-03-16 14:02:58 +0530

    Update config.py
    Signed-off-by: SHIVA <151495276+FRANCIUM444@users.noreply.github.com>

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "ᴀɴᴏɴ ダ ᴍᴜsɪᴄ")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1356469075").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7530506186").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/official-royal/SONA_
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/OFFICIAL_ROYAL_NETWORK")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/ROYAL_CHATTING")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Etox_stuffs")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/https://t.me/+9LC0gFnLbVg4NTg9")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
